4bebd21370332ffdbda447d73ae734df1643741d
= <=>
2025-08-10 15:00:53 +0530

    fix: correct airplane creation logic and enhance error handling

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -1,15 +1,21 @@
 import { AirPlaneRepository } from "../repositories/index.js";
 import AppError from './../utils/errors/app-errors.js';
 import { StatusCodes } from "http-status-codes";
+
 export async function createAirPlane(data) {
     try {
-        const res = new AirPlaneRepository().creat(data);
+        const res = await new AirPlaneRepository().create(data);
         return res;
     } catch (error) {
-        if (error.name === 'TypeError') {
-            throw new AppError('Cannot create a new Airplane Object', StatusCodes.INTERNAL_SERVER_ERROR)
+        if (error.name === 'SequelizeValidationError') {
+            let explanation = [];
+            error.errors.forEach(err => {
+                explanation.push(err.message);
+            });
+            console.log(explanation);
+            throw new AppError(explanation, StatusCodes.BAD_REQUEST);
         }
-        console.log(error)
-        throw error;
+        throw new AppError('Cannot create a new Airplane Object', StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
+
